72fed7fd2ef7959984300ad3552823eb55b21584
Vishnu V P <vishnuvayalilepurayil@gmail.com>
2023-12-17 21:14:57 +0530

    Improve Routes Structure and add Dashboard Layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, Router } from "react-router-dom"
 import Toggle from "../components/Toggle"
 
 
@@ -7,21 +7,31 @@ const Login = () =>{
     
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [organization_id, setOrganization_id] = useState('')
     const [role, setRole] = useState('employee') 
         
     const handleSignIn = async (e) => {
         e.preventDefault();
-        const data = { username, password, role };
+        const data = { username, password, organization_id, role };
         console.log(data);
 
-        const response = await fetch('http://localhost:3001/login', {
+        const res = await fetch('http://localhost:3001/login', {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json'},
+            credentials: 'include',
             body: JSON.stringify(data)
         });
 
-        const result = await response.json();
+        const result = await res.json();
         console.log(result);
+
+        if (result!= "failure") {
+            alert("Login Successful");
+            //localStorage.setItem('token', cookies.jwt);
+            <Router className="push"></Router>
+        } else {
+            alert("Invalid credentials");
+        }
 
        
         
@@ -31,25 +41,25 @@ const Login = () =>{
 
     return(
         <section className='relative z-[99] my-6 flex justify-center w-screen'>
-            <div className="w-3/4 md:max-w-lg sm:max-w-md rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
-                <div className="p-8">
-                    <h1 className=" mb-6 text-xl text-center font-semibold  text-gray-900 md:text-2xl dark:text-white">
+            <div className="w-3/4 md:max-w- sm:max-w-md rounded-lg shadow dark:border dark:bg-gray-800 dark:border-gray-700">
+                <div className="p-6">
+                    <h1 className=" mb-6 text-lg text-center font-semibold  text-gray-900 md:text-2xl dark:text-white">
                         Hello Again!
                     </h1>
                     <Toggle onRoleChange={setRole}/>
-                    <form className="space-y-4" onSubmit={handleSignIn}>
+                    <form className="space-y-2" onSubmit={handleSignIn}>
                         <div>
-                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email or username</label>
+                            <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username or email</label>
                             <input type="text" name="email" id="email" onChange={e => setUsername(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring--600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
                         </div>
                         <div>
                             <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                             <input type="password" name="password" id="password" placeholder="••••••••" onChange={e => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                         </div>
-                        {/* <div>
+                        <div>
                             <label for="organization_id" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Organization ID</label>
-                            <input type="text" name="organization_id" id="organization_id"  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="GJfgQ521Op" required=""/>
-                        </div> */}
+                            <input type="text"  name="organization_id" id="organization_id" onChange={e => setOrganization_id(e.target.value)}  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="GJfgQ521Op" required=""/>
+                        </div> 
                         <div className="flex items-center justify-end">
                             
                             <Link to="#" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</Link>
