80eac4fe31fb5cf780f01415be211903a9ca872a
Mohammed Razik R <razikrehim123@gmail.com>
2023-12-17 21:40:05 +0530

    dasboard-and-related components are added(bug_dashbaord component needs to be separated from the main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import React from 'react'
+import React from 'react'
 import Navbar from './pages/Navbar.jsx'
 import About from './pages/About.jsx'
 import Footer from './pages/Footer.jsx'
@@ -14,7 +14,7 @@ import Dashboard from './pages/Dashboard.jsx'
 
 const App = () =>(
   <>
-    <div className=" min-h-screen bg-slate-950 overflow-hidden flex flex-col">
+    <div className=" font-[poppins] min-h-screen bg-slate-950 overflow-hidden flex flex-col">
     <Navbar/>
     <div className="container">
         <Routes>
@@ -37,5 +37,8 @@ const App = () =>(
 
 export default App
 
+
 {/* //<div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div></div>
 //<div class="min-h-screen bg-slate-950"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div> */}
+
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,48 @@
 
+import React from 'react';
+import Sidebar
+ from './dashboard/Sidebar';
+ import { SidebarItem } from './dashboard/Sidebar';
+import Navbar from './dashboard/Navbar';
+import{
+  LayoutDashboard,MessagesSquare,ClipboardList,CalendarDays,Users,Settings,FolderOpenDot
+} from "lucide-react"
+import Home from './dashboard/Home';
+import { Route, Routes, Navigate } from 'react-router-dom'
+
 const Dashboard = () => {
     return (
-        <div>
-        <h1 className="border-2">Dashboard</h1>
-        </div>
+        <div className="h-screen  max-w-full  flex p-3  bg-gradient-to-t from-black via-black to-gray-900">
+   
+    <Sidebar>
+     <SidebarItem icon={<LayoutDashboard size={22} color='black'/> } text="Dashboard" active />
+
+     <SidebarItem icon={<FolderOpenDot  size={22} color='white'/> } text="Projects" active />
+
+     <SidebarItem icon={<ClipboardList size={22} color='white'/> } text="tasks" active />
+     <SidebarItem icon={<MessagesSquare  size={22} color='white'/> } text="Chat" active />
+     <SidebarItem icon={<CalendarDays  size={22} color='white'/> } text="Calendar" active />
+     <SidebarItem icon={<LayoutDashboard  size={22} color='white'/> } text="Members" active />
+     <SidebarItem icon={<Settings  size={22} color='white'/> } text="Settings" active />
+
+
+
+    </Sidebar> 
+
+    <div className=' flex-1 w-full '>
+  <Navbar />
+
+ 
+        <Routes>
+          <Route path="/" element = {<Home/>} />
+          <Route path="/settings"  />
+        
+        </Routes>
+    
+
+  </div>
+
+   </div>
     );
 }
 
